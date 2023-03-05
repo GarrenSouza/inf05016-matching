8d07c46dffa12bdc17b442717bb2c06475ec62df
gsouza-mbochip <garrenlus.souza@mbochip.com>
2023-03-05 13:23:16 -0300

    wip: initial commit

---
+++ include/test.hpp
@@ -0,0 +1,6 @@
+
+#include <string>
+
+auto whisper() -> std::string{
+    return "I come from the other side (header files), good news so far!";
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <test.hpp>
+#include <graph.hpp>
+
+int main(){
+    std::cout << whisper() << '\n';
+    return 0;
+}
