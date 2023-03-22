fe9608fc570ea7dd62423fb9ecd73b1807314551
gsouza-mbochip <garrenlus.souza@mbochip.com>
2023-03-22 00:01:01 -0300

    feat: beta

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <graph.hpp>
 
-int main(){
+int main() {
 
     local::BipCompGraph g(std::cin);
-    std::cout << g << '\n';
+    std::vector<int> m(g.nodes_count, static_cast<int>(local::MagicNodes::NULL_NODE));
+    auto matching_sum = g.getMaximumMatching(m);
+//    std::cout << g << '\n';
+    std::cout << "Maximum match sum is: " << g.expected_maximum_match << '\n';
+    std::cout << "Maximum match sum found: " << matching_sum << '\n';
+    for (int i = 0; i < g.nodes_count / 2; ++i)
+        std::cout << i << " is matched with " << m[i] << '\n';
     return 0;
 }
