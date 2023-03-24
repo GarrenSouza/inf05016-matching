4ab0094591f01d4f0be40b03f21f4038b820fff0
gsouza-mbochip <garrenlus.souza@mbochip.com>
2023-03-24 12:58:57 -0300

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,18 @@
 
 int main() {
 
-    local::BipCompGraph g(std::cin);
+    std::function<int32_t(int32_t)> transformation = [](int32_t i) -> int32_t { return -i; };
+    local::BipCompGraph g(std::cin, transformation);
     std::vector<int> m(g.nodes_count, static_cast<int>(local::MagicNodes::NULL_NODE));
-    auto matching_sum = g.getMaximumMatching(m);
+    g.getMaximumPerfectMatching(m);
 //    std::cout << g << '\n';
-    std::cout << "Maximum match sum is: " << g.expected_maximum_match << '\n';
-    std::cout << "Maximum match sum found: " << matching_sum << '\n';
+    int32_t sum = 0;
     for (int i = 0; i < g.nodes_count / 2; ++i)
-        std::cout << i << " is matched with " << m[i] << '\n';
+        if (m[i] != static_cast<int32_t>(local::MagicNodes::NULL_NODE)) {
+            std::cout << (i + 1) << " is matched with " << (m[i] + 1) << ": " << g.get_weight_raw(i, m[i]) << '\n';
+            sum += g.get_weight_raw(i, m[i]);
+        } else
+            std::cout << i << " is not matched\n";
+    std::cout << "Maximum matching is: " << sum << '\n';
     return 0;
 }
